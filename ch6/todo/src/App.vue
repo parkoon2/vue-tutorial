939b70c9df19ea90361b90c406652bfc3e07d521
parkoon <par_koon@naver.com>
2019-09-09 10:09:17 +0900

    todo

--- ch6/todo/src/App.vue
+++ ch6/todo/src/App.vue
@@ -1,75 +1,36 @@
 <template>
-    <div id="app">
-        <div class="container">
-            <div class="col-md-6 col-md-offset-3">
-                <h1 class="text-center">투 두 리스트</h1>
-                <input type="text" class="form-control" v-model="todoInput" @keyup.enter="addTodo" />
-                <div class="list-group">
-                    <template v-for="todo in activeTodoList">
-                        <!-- <button
-                            class="list-group-item text-left"
-                            :key="todo.id"
-                            @click="toggleTodoState(todo)"
-                        >{{ todo.label }}</button>-->
-                        <Todo :label="todo.label" @componentClick="toggleTodoState(todo)"></Todo>
-                    </template>
-                </div>
+  <v-app>
+    <v-app-bar app>
+      <v-toolbar-title class="headline text-uppercase">
+        <span>Vuetify</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </v-toolbar-title>
+      <v-spacer></v-spacer>
+      <v-btn
+        text
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+      >
+        <span class="mr-2">Latest Release</span>
+      </v-btn>
+    </v-app-bar>
 
-                <div class="text-right">
-                    <button class="btn btn-sm" @click="changeCurrentState('active')">할 일</button>
-                    <button class="btn btn-sm" @click="changeCurrentState('done')">완료</button>
-                    <button class="btn btn-sm" @click="changeCurrentState('all')">전체</button>
-                </div>
-            </div>
-        </div>
-    </div>
+    <v-content>
+      <HelloWorld/>
+    </v-content>
+  </v-app>
 </template>
 
 <script>
-import Todo from './components/Todo'
+import HelloWorld from './components/HelloWorld';
 
-let id = 0
 export default {
-    name: 'app',
-    data() {
-        return {
-            todoInput: '',
-            todoList: [],
-            currentState: 'active'
-        }
-    },
-
-    computed: {
-        activeTodoList() {
-            return this.todoList.filter(
-                todo =>
-                    this.currentState === 'all' ||
-                    todo.state === this.currentState
-            )
-        }
-    },
-    methods: {
-        changeCurrentState(state) {
-            this.currentState = state
-        },
-        addTodo(e) {
-            let todo = {
-                id: id++,
-                label: e.target.value,
-                state: 'active'
-            }
-            this.todoList.push(todo)
-            this.todoInput = ''
-        },
-        toggleTodoState(todo) {
-            todo.state = todo.state === 'active' ? 'done' : 'active'
-        }
-    },
-    components: {
-        Todo
-    }
-}
+  name: 'App',
+  components: {
+    HelloWorld,
+  },
+  data: () => ({
+    //
+  }),
+};
 </script>
-
-<style>
-</style>
